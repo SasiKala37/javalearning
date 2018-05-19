6aa50f89741e82586fa2fd476ca2bcd6094fc884
bridgelabz <SasiKala37>
2018-05-19 14:45:23 +0530

    Gambler

--- src/com/bridgelabz/functional/Gambler.java
+++ src/com/bridgelabz/functional/Gambler.java
@@ -1,17 +1,25 @@
+/* 
+* Purpose: prints the win and loss percentage of gambler
+* 
+* @author SasiKala
+* @version 1.0
+* @since 18-05-2018
+* 
+*/
 package com.bridgelabz.functional;
 
 import com.bridgelabz.utility.Utility;
 
 public class Gambler {
 	public static void main(String[] args) {
-		Utility utility=new Utility();
+		Utility utility = new Utility();
 		System.out.println("Enter stake");
-		int stake=utility.userInputInteger();
+		int stake = utility.userInputInteger();
 		System.out.println("Enter goal");
-		int goal=utility.userInputInteger();
+		int goal = utility.userInputInteger();
 		System.out.println("Enter number of trails");
-		int numberOfTrails=utility.userInputInteger();
-		
+		int numberOfTrails = utility.userInputInteger();
+
 		Utility.gamblGoal(stake, goal, numberOfTrails);
 	}
 }
