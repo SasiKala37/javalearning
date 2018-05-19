c0993ed0f934b8f217bb4c1c942d9e4e54cbdb3d
bridgelabz <SasiKala37>
2018-05-19 14:54:50 +0530

    Permutation of String

--- src/com/bridgelabz/functional/PermutationalString.java
+++ src/com/bridgelabz/functional/PermutationalString.java
@@ -1,14 +1,22 @@
+/**
+* Purpose:print all permutations of string
+* 
+* @author SasiKala
+* @version 1.0
+* @since 18-05-2018
+* 
+*/
 package com.bridgelabz.functional;
 
 import com.bridgelabz.utility.Utility;
 
 public class PermutationalString {
 	public static void main(String[] args) {
-		Utility utility=new Utility();
+		Utility utility = new Utility();
 		System.out.println("Enter the input String");
-		String input=utility.userInputString();
-		int lenthOfInput=input.length();
-		
-		Utility.permutation(input, 0, lenthOfInput-1);
+		String input = utility.userInputString();
+		int lenthOfInput = input.length();
+
+		Utility.permutation(input, 0, lenthOfInput - 1);
 	}
 }
