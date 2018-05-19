395ce5ee196b264e203819fdad876a26ea630f44
bridgelabz <SasiKala37>
2018-05-19 15:16:21 +0530

    Triplet Sum

--- src/com/bridgelabz/functional/Triplets.java
+++ src/com/bridgelabz/functional/Triplets.java
@@ -1,13 +1,21 @@
+/**
+* Purpose: Print the triplet sum 
+*  
+* @author SasiKala
+* @version 1.0
+* @since 19-05-2018
+* 
+*/
 package com.bridgelabz.functional;
 
 import com.bridgelabz.utility.Utility;
 
 public class Triplets {
 	public static void main(String[] args) {
-		Utility utility=new Utility();
-		
+		Utility utility = new Utility();
+
 		System.out.println("Enter the triplet sum");
-		int tripletSum=utility.userInputInteger();
+		int tripletSum = utility.userInputInteger();
 		utility.sumOfTriplets(tripletSum);
 	}
 }
