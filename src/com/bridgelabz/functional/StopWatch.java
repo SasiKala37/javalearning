66868f32e88002088f67a9fa5bc7625f3ab1dacd
bridgelabz <SasiKala37>
2018-05-19 15:09:05 +0530

    StopWatch

--- src/com/bridgelabz/functional/StopWatch.java
+++ src/com/bridgelabz/functional/StopWatch.java
@@ -1,3 +1,11 @@
+/**   
+ * Purpose: Prints the elapsed time of stop watch
+ * 
+ * @author  SasiKala
+ * @version 1.0
+ * @since   18-05-2018
+ * 
+ */
 package com.bridgelabz.functional;
 
 import com.bridgelabz.utility.Utility;
